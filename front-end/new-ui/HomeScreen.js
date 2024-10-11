e2c5ddb9fbfa5f66c8fc677ca7471a4ed0a028c2
Qingyang_Li <agecspnt@gmail.com>
2024-10-11 16:14:46 -0400

    added background img to homepage and login page

--- front-end/new-ui/HomeScreen.js
+++ front-end/new-ui/HomeScreen.js
@@ -1,22 +1,44 @@
 /* eslint-disable */
 import * as React from 'react';
-import { View, ScrollView, Image, StyleSheet} from 'react-native';
+import { View, ImageBackground, ScrollView, Image, StyleSheet} from 'react-native';
 import { Text } from 'react-native-paper';
 
 
 
 const HomeScreen = ({navigation}) => {
     return(
-            <View>
-                <Text variant="headlineLarge">Babycare Mobile Web App</Text>
-                <Text>Welcome to our team's babycare web mobile app!</Text>
-                <Text>Please take a minute to use our application for all of your
-                    pregnancy and parental needs!!!
-                </Text>
-            </View>
+            <ImageBackground
+              source={require('./assets/bg3.png')}
+              style={styles.background}
+            >
+                <View>
+                    <Text variant="headlineLarge">Babycare Mobile Web App</Text>
+                    <Text style={{ fontSize: 20 }}>Welcome!</Text>
+                    <Text style={{ fontSize: 20 }}>
+                      Please take a minute to use our application for all of your
+                      pregnancy and parental needs!!!
+                    </Text>
+                </View>
+            </ImageBackground>
     );
 };
 
-
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
 
 export default HomeScreen;
--- front-end/new-ui/Login.js
+++ front-end/new-ui/Login.js
@@ -1,42 +1,62 @@
-/* eslint-disable */
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, ImageBackground} from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { Avatar, Button, Card, Text } from 'react-native-paper';
-
 
 const LoginPage = () => {
     const [username,setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     return(
-        <Card>
-            <Card.Title title="Log in"/>
-            <Card.Content>
-                <TextInput
-                  label={"Username"}
-                  placeholder="Input your username"
-                  onChangeText={newUsername => setUsername(newUsername)}
-                  defaultValue={username}
-                />
-            </Card.Content>
+        <ImageBackground
+            source={require('./assets/bg3.png')}
+            style={styles.background}
+        >
+            <View style={styles.container}>
+                <Card>
+                    <Card.Title title="Log in"/>
+                    <Card.Content>
+                        <TextInput
+                          label={"Username"}
+                          placeholder="Input your username"
+                          onChangeText={newUsername => setUsername(newUsername)}
+                          defaultValue={username}
+                        />
+                    </Card.Content>
 
-            <Card.Content>
-                <TextInput
-                label={"Password"}
-                placeholder="Input your password"
-                onChangeText={newPassword => setPassword(newPassword)}
-                defaultValue = {password}
-                />
-            </Card.Content>
-            <Card.Actions>
-              <Button mode="default" onPress={() => console.log("Username is: " + username + ". Password is: " + password + ".")}>
-                Login
-              </Button>
-            </Card.Actions>
-
-        </Card>
+                    <Card.Content>
+                        <TextInput
+                        label={"Password"}
+                        placeholder="Input your password"
+                        onChangeText={newPassword => setPassword(newPassword)}
+                        defaultValue = {password}
+                        />
+                    </Card.Content>
+                    <Card.Actions>
+                      <Button mode="default" onPress={() => console.log("Username is: " + username + ". Password is: " + password + ".")}>
+                        Login
+                      </Button>
+                      <Button mode="default" onPress={() => console.log("Navigate to Register Page")}>
+                        Register
+                      </Button>
+                    </Card.Actions>
+                </Card>
+            </View>
+        </ImageBackground>
     );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default LoginPage;
