eb720fd5c297c0acf686e543d0930c4460077d21
TheProgramMaster <gavinlaneky@gmail.com>
2024-11-04 14:39:02 -0500

    Created Video Call web page. Created Conference, VideoChatRoomProvider, Peer, and Footer Components as well as CSS styling sheet.

---
+++ front-end/new-ui/VideoChatRoomProvider.js
@@ -0,0 +1,39 @@
+import { StrictMode, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { HMSRoomProvider, useHMSActions } from "@100mslive/react-sdk";
+import JoinFromStyles from './JoinFormStyles.css';
+import JoinForm from './JoinForm';
+import Header from "./Header";
+import Conference from "./Conference";
+import Footer from './Footer';
+import {
+    selectIsConnectedToRoom,
+    useHMSStore
+} from "@100mslive/react-sdk";
+
+const VideoChatRoomProvider = () => {
+    const isConnected = useHMSStore(selectIsConnectedToRoom);
+    const hmsActions = useHMSActions();
+    useEffect(() => {
+        window.onunload = () => {
+            if (isConnected){
+                hmsActions.leave();
+            }
+        };
+    }, [hmsActions, isConnected]);
+    return(
+        <div className="App">
+            <Header />
+            {isConnected ? (
+                <>
+                    <Conference />
+                    <Footer />
+                </>
+            ) : (
+                    <JoinForm />
+            )}
+        </div>
+    );
+};
+
+export default VideoChatRoomProvider;
